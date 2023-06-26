fe83f5cb11a846b7b77f6cd734fac667b6684ebd
unknown <st2019285@g.ryukei.ed.jp>
2023-06-26 13:42:21 +0900

    制御構文の解答

--- prime_number.py
+++ prime_number.py
@@ -2,3 +2,19 @@ a = input("aの値を入力: ")
 b = input("bの値を入力: ")
 
 # TODO
+a = input("aの値を入力: ")
+b = input("bの値を入力: ")
+x = int(a)
+for i in range(2,x):
+  if (x%i==0):
+    print("これは素数ではありません")
+    break
+else:
+    print("これは素数です")
+y = int(b)
+for i in range(2,y):
+  if (y%i==0):
+    print("これは素数ではありません")
+    break
+else:
+    print("これは素数です")
